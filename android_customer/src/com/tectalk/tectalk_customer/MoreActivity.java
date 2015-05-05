feb9780a114eefee0a666f956c1f8c927a6f8315
siyoung <siyoung@10.20.54.194>
2015-05-05 19:58:25 +0900

    로그인 후 리스트 뷰로 상품명 가져옴(리스트 뷰) + 상풍명 클릭시 해당 상품 배송 담당 택배기사님 정보 화면출력

--- android_customer/src/com/tectalk/tectalk_customer/MoreActivity.java
+++ android_customer/src/com/tectalk/tectalk_customer/MoreActivity.java
@@ -16,82 +16,57 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.internal.widget.AdapterViewCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class CustomerActivity extends ActionBarActivity {
-	String abc = "123";
-
+public class MoreActivity extends ActionBarActivity {
 	private Intent intent;
-	private String cus_id;
-	private String url = "http://182.162.90.100/TecTalk/GetItemInfo";
-	private String result;
-	private String text = "";
-	private String item_info = "";
 
-	private JSONArray jArray;
-	private JSONObject jObject;
+	private String cus_id;
+	private String item_info;
+	private String url_get = "http://182.162.90.100/TecTalk/GetDriInfo";
+	private String url_set = "http://182.162.90.100/TecTalk/SetDriInfo";
+	private String result_get;
+	private String result_set;
+	private String text = "";
+	private String text_set = "";
+	
+	private TextView txtViewResult;
+
+	private JSONArray jArray_get;
+	private JSONObject jObject_get;
+	private JSONArray jArray_set;
+	private JSONObject jObject_set;
 
 	private ListView listViewResult;
 	private ArrayAdapter<String> itemAdapter;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_customer);
+		setContentView(R.layout.activity_more);
 
 		itemAdapter = new ArrayAdapter<String>(getApplicationContext(),
 				R.layout.activity_customer2);
-		
+
 		listViewResult = (ListView) findViewById(R.id.listViewResult);
 
 		listViewResult.setAdapter(itemAdapter);
-		
-		listViewResult.setOnItemClickListener(onClickListItem);
 
 		intent = getIntent();
 		cus_id = intent.getExtras().getString("cus_id");
+		item_info = intent.getExtras().getString("item_info");
 
 		new ConnectServer().execute(null, null, null);
-
 	}
-	
-	private OnItemClickListener onClickListItem = new OnItemClickListener(){
-
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id) {
-			
-			item_info = itemAdapter.getItem(position);
-			
-			Intent intent_item = new Intent(getApplicationContext(), MoreActivity.class);
-		
-			intent_item.putExtra("cus_id",cus_id);
-			intent_item.putExtra("item_info",item_info);
-			startActivity(intent_item);
-			finish();
-			
-			 
-		}
-
-		
-		
-	};
-	
-	
 
 	private class ConnectServer extends AsyncTask<Void, Void, Void> {
 
@@ -100,8 +75,10 @@ public class CustomerActivity extends ActionBarActivity {
 			// TODO Auto-generated method stub
 
 			HttpClient client = new DefaultHttpClient();
-			List<NameValuePair> values = new ArrayList<NameValuePair>();
-			values.add(new BasicNameValuePair("CUSTOMER_ID", cus_id));
+			List<NameValuePair> values_get = new ArrayList<NameValuePair>();
+			List<NameValuePair> values_set = new ArrayList<NameValuePair>();
+			values_get.add(new BasicNameValuePair("CUSTOMER_ID", cus_id));
+			values_get.add(new BasicNameValuePair("ITEM_INFO", item_info));
 
 			HttpParams param = client.getParams();
 			HttpConnectionParams.setConnectionTimeout(param, 5000);
@@ -109,11 +86,46 @@ public class CustomerActivity extends ActionBarActivity {
 
 			try {
 
-				url = url + "?" + URLEncodedUtils.format(values, "UTF-8");
-				HttpGet httpGet = new HttpGet(url);
+				url_get = url_get + "?"
+						+ URLEncodedUtils.format(values_get, "UTF-8");
+				HttpGet httpGet = new HttpGet(url_get);
+				HttpGet httpSet = new HttpGet(url_set);
+				HttpResponse response_get = client.execute(httpGet);
 
-				HttpResponse response = client.execute(httpGet);
-				result = EntityUtils.toString(response.getEntity());
+				result_get = EntityUtils.toString(response_get.getEntity());
+				// JSONObject jObject = new JSONObject(result);
+				jArray_get = new JSONArray(result_get);
+				jObject_get = new JSONObject();
+				for (int i = 0; i < jArray_get.length(); i++) {
+					jObject_get = jArray_get.getJSONObject(i);
+					text = jObject_get.getString("dri_info");
+					Log.d("aaa", "text : " + text);
+				}
+				
+				Log.d("aaa", "cus result : " + result_get);
+				
+				values_set.add(new BasicNameValuePair("DRIVER_INFO", text));
+
+				url_set = url_set + "?"
+						+ URLEncodedUtils.format(values_set, "UTF-8");
+
+				HttpResponse response_set = client.execute(httpSet);
+				
+				result_set = EntityUtils.toString(response_set.getEntity());
+				jArray_set = new JSONArray(result_set);
+				jObject_set = new JSONObject();
+				
+				for (int i = 0; i < jArray_set.length(); i++) {
+					jObject_set = jArray_set.getJSONObject(i);
+					text_set += jObject_set.getString("dri_id");
+					text_set += jObject_set.getString("dri_name");
+					text_set += jObject_set.getString("dri_phone");
+					text_set += jObject_set.getString("dri_company");
+					Log.d("aaa", "text : " + text_set);
+				}
+				
+
+				Log.d("aaa", "text : " + text);
 
 				Log.d("aaa", "text : " + text);
 			} catch (Exception e) {
@@ -127,24 +139,9 @@ public class CustomerActivity extends ActionBarActivity {
 		protected void onPostExecute(Void result_) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result_);
-
-			try {
-				// JSONObject jObject = new JSONObject(result);
-				jArray = new JSONArray(result);
-				jObject = new JSONObject();
-				Log.d("aaa", "cus result : " + result);
-
-				for (int i = 0; i < jArray.length(); i++) {
-					jObject = jArray.getJSONObject(i);
-					text = jObject.getString("item_info");
-					itemAdapter.add(text);
-					Log.d("aaa", "text : " + text);
-				}
-			} catch (Exception e) {
-			}
+			txtViewResult.setText(text_set);
 
 		}
-
 	}
 
 	@Override
